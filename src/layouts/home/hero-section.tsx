c1cb744450961bb516d762f73946712b3faae15d
Johurul Haque <johurulhaquejony@gmail.com>
2024-02-15 15:33:01 +0600

    design refactoring

--- src/layouts/home/hero-section.tsx
+++ src/layouts/home/hero-section.tsx
@@ -43,7 +43,7 @@ export function HeroSection() {
       <Header />
 
       <section className="flex relative flex-col items-center mt-20">
-        <div className="flex gap-4 justify-end ml-6 w-36 max-w-full">
+        <div className="flex gap-4">
           {socials.map(({ Icon, url, label }) => (
             <Link key={url} href={url}>
               <span className="sr-only">{label}</span>
@@ -55,20 +55,23 @@ export function HeroSection() {
           ))}
         </div>
 
-        <h1 className="text-white mt-10 text-8xl text-center leading-[110px] max-md:text-4xl max-md:leading-10">
+        <h1 className="text-white font-medium mt-10 text-8xl text-center leading-[110px] max-md:text-4xl max-md:leading-10">
           Create Amazing <br /> websites with IonicByte
         </h1>
-        <p className="text-white mt-4 mb-14 tracking-wider font-light text-center">
+        <p className="text-white mt-4 tracking-wider text-lg font-light text-center">
           We will help you to build websites that is modern, user friendly with
           clean design
         </p>
 
         <Link
           href="/get-started"
-          className="px-5 py-2.5 flex items-center rounded-full gap-2.5 mx-auto bg-white font-medium"
+          className="px-5 py-2.5 mt-12 flex items-center rounded-full gap-2.5 bg-white font-medium group"
         >
           Get Started
-          <ArrowRightCircle className="-rotate-45" strokeWidth={1.5} />
+          <ArrowRightCircle
+            className="-rotate-45 group-hover:rotate-0 transition-transform"
+            strokeWidth={1.5}
+          />
         </Link>
       </section>
     </div>
