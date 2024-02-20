34ca4108cdefe1d4aa8e88611899e17d7be8f907
Johurul Haque <johurulhaquejony@gmail.com>
2024-02-20 13:16:41 +0600

    changed thumbnail of chow-heaven project and getting description of projects from the projects.ts file

--- src/assets/data/projects.ts
+++ src/assets/data/projects.ts
@@ -5,27 +5,35 @@ type Projects = {
   name: string;
   url: string;
   img: StaticImageData;
+  description: string;
 }[];
+
+const description =
+  'Mancoding is a biggest company in Indonesia, which provides the best services in developing websites using Shopify and Wordpress.';
 
 export const projects: Projects = [
   {
     name: 'Phone Fusion Hub',
     url: 'https://phonefusion.netlify.app/',
     img: fusionHub,
+    description,
   },
   {
     name: 'MunchSpot',
     url: 'https://munchspot.netlify.app/',
     img: munchspot,
+    description,
   },
   {
     name: 'Chow Haven',
     url: 'https://chowhaven.netlify.app/',
     img: chowheaven,
+    description,
   },
   {
     name: 'Headphone Showcasing site',
     url: 'https://headset3.netlify.app/',
     img: headphonesSite,
+    description,
   },
 ];
--- src/layouts/home/recent-works.tsx
+++ src/layouts/home/recent-works.tsx
@@ -1,36 +1,20 @@
 import { projects } from '@/assets/data/projects';
 import { BlurryColor } from '@/components/blurry-color';
 import { Heading2 } from '@/components/heading-two';
-import { ArrowRightCircle } from 'lucide-react';
 import Image from 'next/image';
 import Link from 'next/link';
-
-const description =
-  'Mancoding is a biggest company in Indonesia, which provides the best services in developing websites using Shopify and Wordpress.';
 
 export function RecentWorks() {
   return (
     <section className="pt-6 text-white container" id="portfolio">
       <Heading2>Recent Works</Heading2>
-      <div className="flex justify-between items-center gap-3 mt-3">
-        <p className="max-w-md md:max-w-xl font-light max-md:text-sm">
-          Some of our past projects we have completed. We help you build modern,
-          user friendly websites with SEO optimization.
-        </p>
-        <Link
-          href="/projects"
-          className="max-sm:text-sm px-4 md:px-5 py-1.5 md:py-2.5 flex items-center rounded-full gap-2.5 bg-white text-gray-950 font-medium group whitespace-nowrap max-sm:hidden"
-        >
-          View All
-          <ArrowRightCircle
-            className="-rotate-45 group-hover:rotate-0 transition-transform"
-            strokeWidth={1.5}
-          />
-        </Link>
-      </div>
+      <p className="max-w-md md:max-w-xl font-light max-md:text-sm mt-3">
+        Some of our past projects we have completed. We help you build modern,
+        user friendly websites with SEO optimization.
+      </p>
 
       <div className="grid md:grid-cols-2 gap-8 mt-10 md:mt-16 relative max-md:[&>*]:border-b md:[&>*:nth-child(n+1):nth-child(-n+2)]:border-b [&>*]:border-[#635189]">
-        {projects.map(({ name, url, img }) => (
+        {projects.map(({ name, url, img, description }) => (
           <article key={url} className="relative z-10" data-aos="fade-up">
             <Link
               href={url}
@@ -42,11 +26,11 @@ export function RecentWorks() {
                 {name}
               </h3>
               <p className="font-light max-md:text-sm line-clamp-3">
-                {description}{' '}
+                {description}
               </p>
               <Image
                 src={img}
-                className="-order-1 aspect-[2.1] object-cover rounded-lg md:rounded-2xl"
+                className="-order-1 aspect-[2.1] object-cover object-top rounded-lg md:rounded-2xl"
                 width={1920}
                 height={914.29}
                 alt={`Homepage of project ${name}`}
